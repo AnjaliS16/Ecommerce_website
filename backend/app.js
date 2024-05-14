4c5bfd1aab18b3f185f6881dbd39945116e2e3dd
AnjaliS16 <154319674+AnjaliS16@users.noreply.github.com>
2024-05-13 23:01:38 -0700

    relation between tables

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
    User.findById("6641dfdb687cb6e3c31d4803")
     .then(user => {
-       req.user = user;
+       req.user = new User(user.name,user.email,user.cart,user._id);
        next();
      })
      .catch(err => console.log(err));
